faa41c3a38920965a3cc784f62c401ba2f884196
Vitalii <vitalii.bryk.99@gmail.com>
2023-12-12 17:21:04 +0200

    add contacts-info

--- src/app/modules/contacts-info/contacts-info.component.ts
+++ src/app/modules/contacts-info/contacts-info.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class ContactsInfoComponent {
 
+  public cardsArr : Array<any> = [
+    {
+      title: "Sales",
+      subtitle: "Take advantage of our great deals",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "PHONE",
+      phoneInfo: "+1 (855) 855-1221",
+    },
+    {
+      title: "24/7 Support",
+      subtitle: "Let the expertise take over uncertainty. 24/7 live support",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "PHONE",
+      phoneInfo: "+1 (855) 855-1221",
+    },
+    {
+      title: "Anything",
+      subtitle: "For everything else, say hello!",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "PHONE",
+      phoneInfo: "+1 (855) 855-1221",
+    },
+    {
+      title: "Feedback",
+      subtitle: "Help us improve our services,leave us a note.",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "",
+      phoneInfo: "",
+    },
+    {
+      title: "Marketing",
+      subtitle: "Let’s create and deliver progress",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "",
+      phoneInfo: "",
+    },
+    {
+      title: "Partners",
+      subtitle: "Become our partner, let’s achieve more together.",
+      email: "E-MAIL",
+      emailInfo: "sales@FlyBusinessDeals.com",
+      phone: "",
+      phoneInfo: "",
+    },
+  ]
 }
