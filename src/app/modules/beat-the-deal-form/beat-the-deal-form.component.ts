232239a2ce95e2da22806ca19b80bef7c844ccc3
Vitalii <vitalii.bryk.99@gmail.com>
2023-12-14 11:34:51 +0200

    add firebase

--- src/app/modules/beat-the-deal-form/beat-the-deal-form.component.ts
+++ src/app/modules/beat-the-deal-form/beat-the-deal-form.component.ts
@@ -56,14 +56,18 @@ export class BeatTheDealFormComponent implements OnInit {
 
   public infoAboutObj = {
     title_1: "BETTer Deal? Let Us Know!",
-    info_1:"",
+    info_1:"At Fly Business Deals, we're committed to providing you with the best possible travel experience at unbeatable prices. If you've come across a lower price for the same flight elsewhere, we want to hear about it!",
     title_2:"How It Works:",
-    info_2:"",
+    info_2: [
+      {num:"1", title:"Fill in the Details", info:"Use the form below to share the details of the better deal you found. Include the website, date, and any relevant information."},
+      {num:"2", title:"Verification Process", info:"Our team will swiftly verify the information provided. We appreciate your assistance in ensuring the accuracy of the details."},
+      {num:"3", title:"Enjoy Additional Benefits", info:"If your claim is verified, not only will you secure the lowest price, but we'll also offer you additional enefits as a token of appreciation."}
+    ],
     title_3:"our Information:",
-    info_3:"",
+    info_3:"<p>•Full Name:</p><p>• Email Address:</p><p> • Phone Number:</p>",
     title_4:"Details of Better Deal:",
-    info_4:"",
+    info_4:"<p>• Competitor's Website:</p><p>• Flight Details:</p><p>• Price Quoted:</p>",
     title_5:"Additional Comments:",
-    info_5:"",
+    info_5:"Feel confident that Fly Business Deals is dedicated to beating the competition and ensuring you get the most value for your journey. Thank you for choosing Fly Business Deals —where we go the extra mile to beat the deal for you",
   }
 }
